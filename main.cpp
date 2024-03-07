09a9794f44515c86ba47c7e1959c3acbdddb6c35
yarno bachmann <minejarno@gmail.com>
2024-03-07 12:33:05 +0100

    Added calibration via interrupts | Added a println to read the reflection sensor values

--- main.cpp
+++ main.cpp
@@ -1,17 +1,20 @@
+//-----------------------------------[Include libraries]----------------------------
+
 #include <Arduino.h>
 #include <Adafruit_NeoPixel.h>
 #ifdef __AVR__
   #include <avr/power.h>
 #endif
 
-// Function pre decleration
+//-----------------------------------[Predeclare functions]-------------------------
+
 void setPixelColor(int pixel, uint8_t red, uint8_t green, uint8_t blue);
 void setServoAngle(int angle);
 void setGripperAngle(int angle);
 void openGripper();
 void closeGripper();
 void calibrateServo();
-void setMotors(int LFWD, int LBACK, int RFWD, int RBACK);
+void setMotors(double LFWD, double LBACK, double RFWD, double RBACK);
 void brakeLight();
 void forwardLight();
 void leftLight();
@@ -25,11 +28,15 @@ void lookStraight();
 void lookLeft();
 void lookRight();
 long measureDistance();
+void calibrateToDriveStraight();
+void leftPulseInterrupt();
+void rightPulseInterrupt();
+
+//-----------------------------------[Declare pins]-------------------------------
 
 #define PIN            13    // Digital pin connected to the Neopixel
 #define NUMPIXELS      4    // Number of Neopixels in your strip
 
-// Pin Configuration
 const int motorLeftBack =   12;   // Motor pin A1
 const int motorLeftFwd =    11;  // Motor pin A2
 const int motorRightFwd =   10;  // Motor pin B1
@@ -38,20 +45,38 @@ const int motorRightBack =  9;   // Motor pin B2
 const int servo = 5; // servo
 const int gripper = 6; // gripper GR
 
-const int triggerPin = 2; // HC-SR04 trigger pin
+const int triggerPin = 8; // HC-SR04 trigger pin
 const int echoPin = 4;    // HC-SR04 echo pin
 
-const int motorLeftRead =   8;   // Arduino A0
-const int motorRightRead =  7;   // Arduino A1
+const int motorLeftRead =   2;   // Arduino A0
+const int motorRightRead =  3;   // Arduino A1
 
 const int numberOfSensors = 8; // Number of sensors used
 int sensorPins[numberOfSensors] = {A0, A1, A2, A3, A4, A5, A6, A7}; // Analog pins for the sensors
 int sensorValues[numberOfSensors]; // Array to store the sensor values
 
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
+
+//-----------------------------------[Variables]---------------------------------
+
+volatile unsigned long leftPulse = 0;  // Stores the pulse width of the left motor
+volatile unsigned long rightPulse = 0; // Stores the pulse width of the right motor
+
+unsigned long startTime;  // Variable to store the start time for calibration
+const int calibrationDuration = 5000;  // Calibration duration in milliseconds
+bool calibrated = false;  // Flag to check if calibration is done
 
 const int stopDistance = 10; // Distance threshold to stop the robot (in cm)
 
-Adafruit_NeoPixel strip = Adafruit_NeoPixel(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
+int interruptLeft = 0;  // Default value for the left interrupt
+int interruptRight = 0;  // Default value for the right interrupt
+
+int rightOffset = 100;  // RightOffset default value set to 100
+int leftOffset = 100;  // LeftOffset default value set to 100
+
+const int reflectionThreshold = 500; // Adjust this threshold based on your sensor's characteristics
+
+//-----------------------------------[Setup function]---------------------------
 
 void setup() {
   strip.begin();  // Initialize the Neopixel strip
@@ -67,42 +92,61 @@ void setup() {
   pinMode(echoPin, INPUT);
   pinMode(servo, OUTPUT);
   pinMode(gripper, OUTPUT);
+  for(int i = 0;i<=7;i++) 
+  {
+    pinMode(sensorPins[i], INPUT);
+  }
+  attachInterrupt(digitalPinToInterrupt(motorLeftRead), leftPulseInterrupt, CHANGE);
+  attachInterrupt(digitalPinToInterrupt(motorRightRead), rightPulseInterrupt, CHANGE);
   Serial.begin(9600);
-   // Calibrate the servo to a known initial position
-  calibrateServo();
+   
+  calibrateServo(); // Calibrate the servo to a known initial position
 }
+
+//-----------------------------------[Loop function]----------------------------
 
 void loop() {
   // Measure distance
-   long distance = measureDistance();
-
+  long distance = measureDistance();
+  // Read reflection sensor values
+  for (int i = 0; i < numberOfSensors; i++) {
+    sensorValues[i] = analogRead(sensorPins[i]);
+  }
+  // Print reflection sensor values to Serial Monitor
+  Serial.print("Reflection Sensor Values: ");
+  for (int i = 0; i < numberOfSensors; i++) {
+    Serial.print(sensorValues[i]);
+    Serial.print(" ");
+  }
+  Serial.println();
+  // Calibrate to drive straight during the initial setup
+  if (!calibrated) {
+    calibrateToDriveStraight();
+    calibrated = true;
+  }
   // Check for obstacles
   if (distance <= stopDistance) {
     driveStop();
-    Serial.println("Obstacle detected");
-
-    // Look left
+    // Look right with delay
     lookRight();
     delay(1000);
-
     // Measure distance again after looking left
     distance = measureDistance();
-
     // If there is still an obstacle on the left, look right and drive right
     if (distance <= stopDistance) {
       lookLeft();
       delay(1000);
+    } else {
+      // Move forward if no obstacle
+      driveForward(2);
+    }
   } else {
     // Move forward if no obstacle
-    driveForward(255);
-    Serial.println("No obstacle detected");
-  }
-  } else {
-    // Move forward if no obstacle
-      driveForward(255);
-      Serial.println("No obstacle detected");
+    driveForward(2);
   }
 }
+
+//-----------------------------------[Neopixel]-------------------------------
 
 // Set color for a specific Neopixel
 void setPixelColor(int pixel, uint8_t red, uint8_t green, uint8_t blue) {
@@ -110,20 +154,11 @@ void setPixelColor(int pixel, uint8_t red, uint8_t green, uint8_t blue) {
   strip.show();
 }
 
-void setServoAngle(int angle) {
-  // Convert the angle to the corresponding PWM signal
-  int pwmValue = map(angle, 0, 180, 544, 2400);
-
-  // Send the PWM signal to the servo
-  digitalWrite(servo, HIGH);
-  delayMicroseconds(pwmValue);
-  digitalWrite(servo, LOW);
-}
+//-----------------------------------[Gripper]--------------------------------
 
 void setGripperAngle(int angle) {
   // Convert the angle to the corresponding PWM signal
   int pwmValue = map(angle, 0, 180, 544, 2400);
-
   // Send the PWM signal to the servo
   digitalWrite(gripper, HIGH);
   delayMicroseconds(pwmValue);
@@ -144,31 +179,115 @@ void closeGripper() {
   setGripperAngle(closeAngle);
 }
 
+//-----------------------------------[Echo Servo]-----------------------------
+
+void setServoAngle(int angle) {
+  // Convert the angle to the corresponding PWM signal
+  int pwmValue = map(angle, 0, 180, 544, 2400);
+  // Send the PWM signal to the servo
+  digitalWrite(servo, HIGH);
+  delayMicroseconds(pwmValue);
+  digitalWrite(servo, LOW);
+}
+
 // Function to calibrate the servo to find the middle position
 void calibrateServo() {
   // Move to the extreme left position
   int rightAngle = 0;
   setServoAngle(rightAngle);
   delay(1000);  // Wait for 1 second to ensure the servo reaches the left position
-
   // Move to the extreme right position
   int leftAngle = 180;
   setServoAngle(leftAngle);
   delay(1000);  // Wait for 1 second to ensure the servo reaches the right position
-
   // Calculate and move to the middle position
   int middleAngle = 75;
   setServoAngle(middleAngle);
   delay(1000);  // Wait for 1 second to ensure the servo reaches the middle position
 }
 
-// Sets motor power to input
-void setMotors(int LFWD, int LBACK, int RFWD, int RBACK) {
-  analogWrite(motorLeftFwd,   LFWD);
-  analogWrite(motorLeftBack,  LBACK);
-  analogWrite(motorRightFwd,  RFWD);
-  analogWrite(motorRightBack, RBACK);
+//-----------------------------------[Puls sensors]---------------------------
+
+void leftPulseInterrupt() {
+  noInterrupts();
+  interruptLeft++;
+  interrupts();
 }
+
+void rightPulseInterrupt() {
+  noInterrupts();
+  interruptRight++;
+  interrupts();
+}
+
+//-----------------------------------[Wheels motor]---------------------------
+
+void calibrateToDriveStraight() {
+  driveForward(2);
+  delay(5000);
+  driveStop();
+  // Stops the interrupts to prevent interference during calibration
+  noInterrupts();
+  // Determine the maximum and minimum pulses from left and right interrupts
+  double maxPulses = max(interruptLeft, interruptRight);
+  double minPulses = min(interruptLeft, interruptRight);
+  // Calculate the offset pulses as a percentage based on the minimum and maximum pulses
+  double offsetPulses = round((minPulses / maxPulses) * 100);
+  // Set the offsets for left and right motors based on the maximum pulses
+  rightOffset = (interruptRight == maxPulses) ? offsetPulses : 100;
+  leftOffset = (interruptLeft == maxPulses) ? offsetPulses : 100;
+  // Print calibration results to Serial Monitor
+  Serial.print("Left offset: ");
+  Serial.print(leftOffset);
+  Serial.print(" | Right offset: ");
+  Serial.println(rightOffset);
+  Serial.println(offsetPulses);
+  Serial.println(maxPulses);
+  Serial.println(minPulses);
+  // Re-enable interrupts 
+  interrupts();
+}
+
+// Sets motor power to input
+void setMotors(double LFWD, double LBACK, double RFWD, double RBACK) {
+  analogWrite(motorLeftFwd,   round(LFWD * leftOffset));
+  analogWrite(motorLeftBack,  round(LBACK * leftOffset));
+  analogWrite(motorRightFwd,  round(RFWD * rightOffset));
+  analogWrite(motorRightBack, round(RBACK * rightOffset));
+}
+
+//-----------------------------------[Drive functions]------------------------
+
+// Rotate left at 0-2 speed
+void driveLeft(int speed) {
+  leftLight();
+  setMotors(0, speed, speed, 0);
+}
+
+// Rotate right at 0-2 speed
+void driveRight(int speed) {
+  rightLight();
+  setMotors(speed, 0 , 0, speed);
+}
+
+// Drive forwards at 0-2 speed
+void driveForward(int speed) {
+  setMotors(speed, 0 , speed, 0);
+  forwardLight(); // Turn on forward lights
+}
+
+// Drive backwards at 0-2 speed
+void driveBack(int speed) {
+  setMotors(0, speed, 0, speed);
+}
+
+// Stop driving
+void driveStop() {
+  setMotors(0, 0, 0, 0);
+  brakeLight(); // Turn on brake lights
+}
+
+//-----------------------------------[Blinking lights]------------------------
 
 void brakeLight() {
   // Set color for each Neopixel individually
@@ -202,36 +321,8 @@ void rightLight() {
   setPixelColor(3, 255, 0, 0);    // Turn off other Neopixels
 }
 
-// Rotate left at 0-255 speed
-void driveLeft(int speed) {
-  leftLight();
-  setMotors(0, speed, speed, 0);
-}
+//-----------------------------------[Echo servo directions]------------------
 
-// Rotate right at 0-255 speed
-void driveRight(int speed) {
-  rightLight();
-  setMotors(speed, 0 , 0, speed);
-}
-
-// Drive forwards at 0-255 speed
-void driveForward(int speed) {
-  setMotors(speed, 0 , speed, 0);
-  forwardLight(); // Turn on forward lights
-}
-
-// Drive backwards at 0-255 speed
-void driveBack(int speed) {
-  setMotors(0, speed, 0, speed);
-}
-
-// Stop driving
-void driveStop(){
-  setMotors(0, 0, 0, 0);
-  brakeLight(); // Turn on brake lights
-}
-
-// Function to close the gripper (move the servo to the closed position)
 void lookStraight() {
   // Adjust the angle value based on your servo's specifications
   int angle = 75;
@@ -242,18 +333,15 @@ void lookLeft() {
   // Adjust the angle value based on your servo's specifications
   int angle = 180;
   setServoAngle(angle);
-
   // Add a delay to give some time for the servo to move (you may need to adjust the delay duration)
   delay(800);
-
-   // Measure distance
-   long distance = measureDistance();
-
+  // Measure distance
+  long distance = measureDistance();
   // Check for obstacles
   if (distance <= stopDistance) {
     lookStraight();
   } else {
-    driveLeft(180);
+    driveLeft(2);
     lookStraight();
   }
 }
@@ -263,23 +351,23 @@ void lookRight() {
   // Adjust the angle value based on your servo's specifications
   int angle = 0;
   setServoAngle(angle);
-
   delay(800);
-
-   // Measure distance
-   long distance = measureDistance();
-
+  // Measure distance
+  long distance = measureDistance();
   // Check for obstacles
   if (distance <= stopDistance) {
     lookLeft();
   } else {
-    driveRight(180);
+    driveRight(2);
     lookStraight();
   }
 }
 
+//-----------------------------------[Echo sensor]------------------------
+
 long measureDistance()
 {
+  // Echo detection to measure the distance
   long duration, distance;
   digitalWrite(triggerPin, LOW);
   delayMicroseconds(2);
