b4e8175d504762b0e54be51b8b486dea26624791
yarno bachmann <minejarno@gmail.com>
2024-03-12 16:02:44 +0100

    Included gripper, gyro and servo functionality from Luca test project

--- main.cpp
+++ main.cpp
@@ -10,11 +10,6 @@
 //-----------------------------------[Predeclare functions]-------------------------
 
 void setPixelColor(int pixel, uint8_t red, uint8_t green, uint8_t blue);
-void setServoAngle(int angle);
-void setGripperAngle(int angle);
-void openGripper();
-void closeGripper();
-void calibrateServo();
 void setMotors(double LFWD, double LBACK, double RFWD, double RBACK);
 void brakeLight();
 void forwardLight();
@@ -32,31 +27,42 @@ long measureDistance();
 void calibrateToDriveStraight();
 void leftPulseInterrupt();
 void rightPulseInterrupt();
+void gripperToggle();
+void gripper(int pulse); 
+void servo(int pulse); 
 
 //-----------------------------------[Declare pins]-------------------------------
 
-#define PIN 13    // Digital pin connected to the Neopixel
-#define NUMPIXELS 4    // Number of Neopixels in your strip
+#define PIN 13  // Digital pin connected to the Neopixel
+#define NUMPIXELS 4 // Number of Neopixels in your strip
 
-#define MOTOR_LEFT_BACKWARD 12   // Motor pin A1
+#define MOTOR_LEFT_BACKWARD 12  // Motor pin A1
 #define MOTOR_LEFT_FORWARD  11  // Motor pin A2
 #define MOTOR_RIGHT_FORWARD 10  // Motor pin B1
-#define MOTOR_RIGHT_BACKWARD 9   // Motor pin B2
+#define MOTOR_RIGHT_BACKWARD 9  // Motor pin B2
 #define MOTOR_LEFT_READ 2   // Arduino A0
-#define MOTOR_RIGHT_READ 3   // Arduino A1
+#define MOTOR_RIGHT_READ 3  // Arduino A1
 
 #define GRIPPER_PIN 6 // gripper GR
-#
+#define GRIPPER_OPEN 1600 // pulse length servo open
+#define GRIPPER_CLOSED  930 // pulse length servo closed
+#define GRIPPER_INTERVAL  20  // time between pulse
+#define GRIPPER_TOGGLE  1000  // toggle gripper every second
 
+#define SERVO_INTERVAL  20  // time between pulse
+#define SERVO_MIN_PULSE  250  // pulse length for -90 degrees
+#define SERVO_MID_PULSE  1400 // pulse length for 0 degrees
+#define SERVO_MAX_PULSE  2400 // pulse length for 90 degrees
 #define SERVO_PIN 5 // servo
 #define TRIGGER_PIN 8 // HC-SR04 trigger pin
-#define ECHO_PIN 4    // HC-SR04 echo pin
+#define ECHO_PIN 4  // HC-SR04 echo pin
 
 const int numberOfSensors = 8; // Number of sensors used
 int sensorPins[numberOfSensors] = {A7 ,A6, A5, A4, A3, A2, A1, A0}; // Analog pins for the sensors
 int sensorValues[numberOfSensors]; // Array to store the sensor values
 
 Adafruit_NeoPixel strip = Adafruit_NeoPixel(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
+Adafruit_LSM6DS3TRC lsm6ds3trc;
 
 //-----------------------------------[Variables]---------------------------------
 
@@ -77,9 +83,6 @@ int leftOffset = 100;  // LeftOffset default value set to 100
 
 const int reflectionThreshold = 500; // Adjust this threshold based on your sensor's characteristics
 
-unsigned long gripperTimer = 0;
-const int gripperDelay = 1000; // Delay in milliseconds
-
 //-----------------------------------[Setup function]---------------------------
 
 void setup() {
@@ -95,7 +98,9 @@ void setup() {
   pinMode(TRIGGER_PIN, OUTPUT);
   pinMode(ECHO_PIN, INPUT);
   pinMode(SERVO_PIN, OUTPUT);
+  digitalWrite(SERVO_PIN, LOW);
   pinMode(GRIPPER_PIN, OUTPUT);
+  digitalWrite(GRIPPER_PIN, LOW);
   for(int i = 0;i<=7;i++) 
   {
     pinMode(sensorPins[i], INPUT);
@@ -103,20 +108,169 @@ void setup() {
   attachInterrupt(digitalPinToInterrupt(MOTOR_LEFT_READ), leftPulseInterrupt, CHANGE);
   attachInterrupt(digitalPinToInterrupt(MOTOR_RIGHT_READ), rightPulseInterrupt, CHANGE);
   Serial.begin(9600);
-   
-  calibrateServo(); // Calibrate the servo to a known initial position
+  gripper(GRIPPER_OPEN);
+
+  while (!Serial)
+    delay(1000); // will pause Zero, Leonardo, etc until serial console opens
+
+  Serial.println("Adafruit LSM6DS3TR-C test!");
+
+  if (!lsm6ds3trc.begin_I2C()) {
+    // if (!lsm6ds3trc.begin_SPI(LSM_CS)) {
+    // if (!lsm6ds3trc.begin_SPI(LSM_CS, LSM_SCK, LSM_MISO, LSM_MOSI)) {
+    Serial.println("Failed to find LSM6DS3TR-C chip");
+    while (1) {
+      delay(1000);
+    }
+  }
+
+  Serial.println("LSM6DS3TR-C Found!");
+
+  // lsm6ds3trc.setAccelRange(LSM6DS_ACCEL_RANGE_2_G);
+  Serial.print("Accelerometer range set to: ");
+  switch (lsm6ds3trc.getAccelRange()) {
+  case LSM6DS_ACCEL_RANGE_2_G:
+    Serial.println("+-2G");
+    break;
+  case LSM6DS_ACCEL_RANGE_4_G:
+    Serial.println("+-4G");
+    break;
+  case LSM6DS_ACCEL_RANGE_8_G:
+    Serial.println("+-8G");
+    break;
+  case LSM6DS_ACCEL_RANGE_16_G:
+    Serial.println("+-16G");
+    break;
+  }
+
+    // lsm6ds3trc.setGyroRange(LSM6DS_GYRO_RANGE_250_DPS);
+  Serial.print("Gyro range set to: ");
+  switch (lsm6ds3trc.getGyroRange()) {
+  case LSM6DS_GYRO_RANGE_125_DPS:
+    Serial.println("125 degrees/s");
+    break;
+  case LSM6DS_GYRO_RANGE_250_DPS:
+    Serial.println("250 degrees/s");
+    break;
+  case LSM6DS_GYRO_RANGE_500_DPS:
+    Serial.println("500 degrees/s");
+    break;
+  case LSM6DS_GYRO_RANGE_1000_DPS:
+    Serial.println("1000 degrees/s");
+    break;
+  case LSM6DS_GYRO_RANGE_2000_DPS:
+    Serial.println("2000 degrees/s");
+    break;
+  case ISM330DHCX_GYRO_RANGE_4000_DPS:
+    break; // unsupported range for the DS33
+  }
+
+  // lsm6ds3trc.setAccelDataRate(LSM6DS_RATE_12_5_HZ);
+  Serial.print("Accelerometer data rate set to: ");
+  switch (lsm6ds3trc.getAccelDataRate()) {
+  case LSM6DS_RATE_SHUTDOWN:
+    Serial.println("0 Hz");
+    break;
+  case LSM6DS_RATE_12_5_HZ:
+    Serial.println("12.5 Hz");
+    break;
+  case LSM6DS_RATE_26_HZ:
+    Serial.println("26 Hz");
+    break;
+  case LSM6DS_RATE_52_HZ:
+    Serial.println("52 Hz");
+    break;
+  case LSM6DS_RATE_104_HZ:
+    Serial.println("104 Hz");
+    break;
+  case LSM6DS_RATE_208_HZ:
+    Serial.println("208 Hz");
+    break;
+  case LSM6DS_RATE_416_HZ:
+    Serial.println("416 Hz");
+    break;
+  case LSM6DS_RATE_833_HZ:
+    Serial.println("833 Hz");
+    break;
+  case LSM6DS_RATE_1_66K_HZ:
+    Serial.println("1.66 KHz");
+    break;
+  case LSM6DS_RATE_3_33K_HZ:
+    Serial.println("3.33 KHz");
+    break;
+  case LSM6DS_RATE_6_66K_HZ:
+    Serial.println("6.66 KHz");
+    break;
+  }
+
+   // lsm6ds3trc.setGyroDataRate(LSM6DS_RATE_12_5_HZ);
+  Serial.print("Gyro data rate set to: ");
+  switch (lsm6ds3trc.getGyroDataRate()) {
+  case LSM6DS_RATE_SHUTDOWN:
+    Serial.println("0 Hz");
+    break;
+  case LSM6DS_RATE_12_5_HZ:
+    Serial.println("12.5 Hz");
+    break;
+  case LSM6DS_RATE_26_HZ:
+    Serial.println("26 Hz");
+    break;
+  case LSM6DS_RATE_52_HZ:
+    Serial.println("52 Hz");
+    break;
+  case LSM6DS_RATE_104_HZ:
+    Serial.println("104 Hz");
+    break;
+  case LSM6DS_RATE_208_HZ:
+    Serial.println("208 Hz");
+    break;
+  case LSM6DS_RATE_416_HZ:
+    Serial.println("416 Hz");
+    break;
+  case LSM6DS_RATE_833_HZ:
+    Serial.println("833 Hz");
+    break;
+  case LSM6DS_RATE_1_66K_HZ:
+    Serial.println("1.66 KHz");
+    break;
+  case LSM6DS_RATE_3_33K_HZ:
+    Serial.println("3.33 KHz");
+    break;
+  case LSM6DS_RATE_6_66K_HZ:
+    Serial.println("6.66 KHz");
+    break;
+  }
+
+  lsm6ds3trc.configInt1(false, false, true); // accelerometer DRDY on INT1
+  lsm6ds3trc.configInt2(false, true, false); // gyro DRDY on INT2
 }
 
 //-----------------------------------[Loop function]----------------------------
 
 void loop() {
- 
-  if (millis() - gripperTimer >= gripperDelay) {
-    openGripper();
-    gripperTimer = millis(); // Reset the timer
-  }
+  // Get a new normalized sensor event
+  sensors_event_t accel;
+  sensors_event_t gyro;
+  sensors_event_t temp;
+  lsm6ds3trc.getEvent(&accel, &gyro, &temp);
 
-    // Measure distance
+  Serial.print(temp.temperature);
+  Serial.print(" Temps,");
+
+  Serial.print(accel.acceleration.x);
+  Serial.print(" Accel.y,"); Serial.print(accel.acceleration.y);
+  Serial.print(" Accel.z,"); Serial.print(accel.acceleration.z);
+  Serial.print(",");
+
+  Serial.print(gyro.gyro.x);
+  Serial.print(" Gyro.y,"); Serial.print(gyro.gyro.y);
+  Serial.print(" Gyro.z,"); Serial.print(gyro.gyro.z);
+  Serial.println();
+  delayMicroseconds(10000);
+
+  gripper(GRIPPER_OPEN);
+
+  // Measure distance
   long distance = measureDistance();
   // Read reflection sensor values
   for (int i = 0; i < numberOfSensors; i++) {
@@ -162,10 +316,7 @@ void loop() {
     // Move forward if no obstacle
     driveForward(2);
   }
-  if (millis() - gripperTimer >= gripperDelay) {
-    closeGripper();
-    gripperTimer = millis(); // Reset the timer
-  }
+  gripper(GRIPPER_CLOSED);
 }
 
 //-----------------------------------[Neopixel]-------------------------------
@@ -178,56 +329,50 @@ void setPixelColor(int pixel, uint8_t red, uint8_t green, uint8_t blue) {
 
 //-----------------------------------[Gripper]--------------------------------
 
-void setGripperAngle(int angle) {
-  // Convert the angle to the corresponding PWM signal
-  int pwmValue = map(angle, 0, 180, 544, 2400);
-  // Send the PWM signal to the servo
-  digitalWrite(GRIPPER_PIN, HIGH);
-  delayMicroseconds(pwmValue);
-  digitalWrite(GRIPPER_PIN, LOW);
+void gripperToggle() {
+  static unsigned long timer;
+  static bool state;
+  if (millis() > timer) {
+    if (state == true) {
+      gripper(GRIPPER_OPEN);
+      state = false;
+    } else {
+      gripper(GRIPPER_CLOSED);
+      state = true;
+    }
+    timer = millis() + GRIPPER_TOGGLE;
+  }
 }
 
-// Function to open the gripper (move the servo to the open position)
-void openGripper() {
-  // Adjust the angle value based on your servo's specifications
-  int openAngle = 80;
-  setGripperAngle(openAngle);
-}
-
-// Function to close the gripper (move the servo to the closed position)
-void closeGripper() {
-  // Adjust the angle value based on your servo's specifications
-  int closeAngle = 30;
-  setGripperAngle(closeAngle);
+void gripper(int pulse) {
+  static unsigned long timer;
+  static int pulse1;
+  if (pulse > 0) {
+    pulse1 = pulse;
+  }
+  if (millis() > timer) {
+    digitalWrite(GRIPPER_PIN, HIGH);
+    delayMicroseconds(pulse1);
+    digitalWrite(GRIPPER_PIN, LOW);
+    timer = millis() + GRIPPER_INTERVAL;
+  }
 }
 
 //-----------------------------------[Echo Servo]-----------------------------
 
-void setServoAngle(int angle) {
-  // Convert the angle to the corresponding PWM signal
-  int pwmValue = map(angle, 0, 180, 544, 2400);
-  // Send the PWM signal to the servo
-  digitalWrite(SERVO_PIN, HIGH);
-  delayMicroseconds(pwmValue);
-  digitalWrite(SERVO_PIN, LOW);
+void servo(int pulse) {
+  static unsigned long timer;
+  static int pulse1;
+  if (pulse > 0) {
+    pulse1 = pulse;
+  }
+  if (millis() > timer) {
+    digitalWrite(SERVO_PIN, HIGH);
+    delayMicroseconds(pulse1);
+    digitalWrite(SERVO_PIN, LOW);
+    timer = millis() + SERVO_INTERVAL;
+  }
 }
-
-// Function to calibrate the servo to find the middle position
-void calibrateServo() {
-  // Move to the extreme left position
-  int rightAngle = 0;
-  setServoAngle(rightAngle);
-  delay(1000);  // Wait for 1 second to ensure the servo reaches the left position
-  // Move to the extreme right position
-  int leftAngle = 180;
-  setServoAngle(leftAngle);
-  delay(1000);  // Wait for 1 second to ensure the servo reaches the right position
-  // Calculate and move to the middle position
-  int middleAngle = 75;
-  setServoAngle(middleAngle);
-  delay(1000);  // Wait for 1 second to ensure the servo reaches the middle position
-}
-
 //-----------------------------------[Puls sensors]---------------------------
 
 void leftPulseInterrupt() {
@@ -346,15 +491,11 @@ void rightLight() {
 //-----------------------------------[Echo servo directions]------------------
 
 void lookStraight() {
-  // Adjust the angle value based on your servo's specifications
-  int angle = 75;
-  setServoAngle(angle);
+  servo(SERVO_MID_PULSE);
 }
 
 void lookLeft() {
-  // Adjust the angle value based on your servo's specifications
-  int angle = 180;
-  setServoAngle(angle);
+  servo(SERVO_MAX_PULSE);
   // Add a delay to give some time for the servo to move (you may need to adjust the delay duration)
   delay(800);
   // Measure distance
@@ -370,9 +511,7 @@ void lookLeft() {
 
 // Function to close the gripper (move the servo to the closed position)
 void lookRight() {
-  // Adjust the angle value based on your servo's specifications
-  int angle = 0;
-  setServoAngle(angle);
+  servo(SERVO_MIN_PULSE);
   delay(800);
   // Measure distance
   long distance = measureDistance();
