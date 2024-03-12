1c9ce66a5bfcbbcda1d0e8b81450c1a3cb8a1d70
Lennon G Spaenij <Lennon.g.spaeny@gmail.com>
2024-03-12 13:14:12 +0100

    Reworked servo after the workshop

--- Piet/Test_servo/Test_servo.ino
+++ Piet/Test_servo/Test_servo.ino
@@ -1,27 +1,54 @@
-const int openPos = 120;   // Open position in degrees
-const int servo = 12;
-
+#define   GRIPPER_PIN     12      // servo pin  
+#define   GRIPPER_OPEN    1800   // pulse length servo open
+#define   GRIPPER_CLOSED  1100    // pulse length servo closed
+#define   SERVO_INTERVAL  20    // time between pulse
+#define   GRIPPER_TOGGLE  1000  // toggle gripper every second
 
 void setup() {
-  pinMode(servo, OUTPUT);  // Set servo pin as an output
-  writeServo(0);
-  delay(500);
+  // put your setup code here, to run once:
+  pinMode(GRIPPER_PIN, OUTPUT);
+  digitalWrite(GRIPPER_PIN, LOW);
 }
 
 void loop() {
-  // Your main loop code here
-  for (int pos = 0; pos <= openPos; pos++) {
-    writeServo(pos);
-    delay(40);
-  }
+  // put your main code here, to run repeatedly:
+  servo(GRIPPER_OPEN);
+  delay(1000);
+  servo(GRIPPER_CLOSED);
+  delay(1000);
 
-  for (int pos = openPos; pos >= 0; pos--) {
-    writeServo(pos);
-    delay(50);
+  servo(gripperToggle);
+  delay(1000);
+  servo(GRIPPER_OPEN);
+  delay(1000);
+  servo(0);
+}
+
+void gripperToggle() {
+  static unsigned long timer;
+  static bool state;
+  if (millis() > timer) {
+    if (state == true) {
+      servo(GRIPPER_OPEN);
+      state = false;
+    } else {
+      servo(GRIPPER_CLOSED);
+      state = true;
+    }
+    timer = millis() + GRIPPER_TOGGLE;
   }
 }
 
-void writeServo(int angle) {
-  int servoAngle = map(angle, 0, openPos, 0, 180);
-  analogWrite(servo, servoAngle);
+void servo(int pulse) {
+  static unsigned long timer;
+  static int pulse1;
+  if (pulse > 0) {
+    pulse1 = pulse;
+  }
+  if (millis() > timer) {
+    digitalWrite(GRIPPER_PIN, HIGH);
+    delayMicroseconds(pulse1);
+    digitalWrite(GRIPPER_PIN, LOW);
+    timer = millis() + SERVO_INTERVAL;
+  }
 }
